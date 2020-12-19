6835dff8640ba569074d5e1ebfef7ef4892336fb
Debarshi Basak <debar5h16a5ak@gmail.com>
2020-12-19 15:56:21 +0100

    Update app.go

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, This is adaptive demo no. 1")
+    fmt.Fprintf(w, "Hi there, This is adaptive demo no. 2")
 }
 
 func main() {
