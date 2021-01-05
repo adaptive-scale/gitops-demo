8365e50462256171b78ffbd731486dcea012b5df
ronakmassand <73260278+ronakmassand@users.noreply.github.com>
2021-01-05 15:27:54 -0500

    Update app.go

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     log.Println("api received")
-    fmt.Fprintf(w, "Hi there, This is adaptive demo no. 20")
+    fmt.Fprintf(w, "Hi there, This is adaptive demo no. 21")
 }
 
 func main() {
