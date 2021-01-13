02b7ddc3d596a50da443d194d6726fccaa66d81a
Debarshi Basak <debar5h16a5ak@gmail.com>
2021-01-14 00:21:00 +0100

    Update app.go

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     log.Println("api received")
-    fmt.Fprintf(w, "Hi there, This is adaptive demo no. 24")
+    fmt.Fprintf(w, "Hi there, This is adaptive demo with env parameters "+os.Getenv("TEST_PARAM_1")+"no. 24")
 }
 
 func main() {
