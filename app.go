1d4571737fbfc7ea38d881b67b6782dee89b3a4c
debarshibasak <debarshri@gmail.com>
2021-02-08 01:59:22 +0100

    ronak changed

--- app.go
+++ app.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    log.Println("api received")
-    fmt.Fprintf(w, "Hi there, This is adaptive demo with env parameters "+os.Getenv("TEST_PARAM_1")+" no. 44")
+	log.Println("api received")
+	fmt.Fprintf(w, "Hi there, This is ronak adaptive demo with env parameters "+os.Getenv("TEST_PARAM_1")+" no. 44")
 }
 
 func main() {
-    log.Println("Starting app for demo for debarshi")
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":80", nil))
+	log.Println("Starting app for demo for debarshi")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
