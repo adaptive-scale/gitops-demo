e8882754a22e81dc9a483dfde3879cfa57293cba
Debarshi Basak <debar5h16a5ak@gmail.com>
2021-02-14 17:28:25 +0100

    Update app.go

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "net/http"
+    "log"
     "os"
 )
 
@@ -12,8 +13,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, This is adaptive demo with env parameters "+os.Getenv("TEST_PARAM_1")+" no. 47")
 }
 
+func errHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("error=triggered triggering error for demo")
+    fmt.Fprintf(w, "Error triggered.")
+}
+
 func main() {
     log.Println("Starting app for demo for debarshi")
     http.HandleFunc("/", handler)
+    http.HandleFunc("/error", errHandler)
+
     log.Fatal(http.ListenAndServe(":80", nil))
 }
